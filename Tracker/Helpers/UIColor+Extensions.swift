318257bd3452a4bcd039669c2ba3cab2e2fa3457
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-03-31 22:40:47 +0700

    feat: launch screen implemented

---
+++ Tracker/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extensions.swift
+//  Tracker
+//
+//  Created by Александр Бекренев on 31.03.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var trackerBlue: UIColor {
+        UIColor(named: "TrackerBlue") ?? .red
+    }
+}
