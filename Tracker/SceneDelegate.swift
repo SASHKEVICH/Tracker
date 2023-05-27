299f417d1258d4f0d62b4628250a8d90df742848
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-05-27 12:54:57 +0700

    feat(onboarding): presenting onboarding on startup

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -13,7 +13,15 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabBarViewController()
+		
+		let onboardingHelper = OnboardingViewControllerHelper()
+		let onboardingPresenter = OnboardingViewPresenter(helper: onboardingHelper)
+		let onboardingViewController = OnboardingViewController()
+		
+		onboardingViewController.presenter = onboardingPresenter
+		onboardingPresenter.view = onboardingViewController
+		
+        window?.rootViewController = onboardingViewController
         window?.makeKeyAndVisible()
     }
 }
