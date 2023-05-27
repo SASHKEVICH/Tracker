a39d383109af9b89133323423fc031a6858fd905
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-05-27 12:51:35 +0700

    feat(assets): onboarding images

---
+++ Tracker/Helpers/UIImage+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage+Extensions.swift
+//  Tracker
+//
+//  Created by Александр Бекренев on 27.05.2023.
+//
+
+import UIKit
+
+extension UIImage {
+	var onboardingFirst: UIImage? {
+		UIImage(named: "Onboarding_1")
+	}
+	
+	var onboardingSecond: UIImage? {
+		UIImage(named: "Onboarding_2")
+	}
+}
