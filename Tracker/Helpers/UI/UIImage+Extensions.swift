1c43a8a8cdc4d78a1560b5eabbb6c4069678e5af
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-07-05 17:59:48 +0700

    feat(images): extension from assets

--- Tracker/Helpers/UI/UIImage+Extensions.swift
+++ Tracker/Helpers/UI/UIImage+Extensions.swift
@@ -9,12 +9,36 @@ import UIKit
 
 extension UIImage {
 	struct Onboarding {
-		static var first: UIImage? {
-			UIImage(named: "Onboarding_1")
-		}
-		
-		static var second: UIImage? {
-			UIImage(named: "Onboarding_2")
-		}
+		static let first = UIImage.image(named: "Onboarding_1")
+		static let second = UIImage.image(named: "Onboarding_2")
+	}
+
+	struct Categories {
+		static let selected = UIImage.image(named: "CategorySelectedIcon")
+	}
+
+	struct MainScreen {
+		static let addTracker = UIImage.image(named: "AddTrackerIcon")
+	}
+
+	struct Launch {
+		static let logo = UIImage.image(named: "PracticumLogo")
+	}
+
+	struct Placeholder {
+		static let emptyForDay = UIImage.image(named: "TrackersPlaceholderViewEmptyForDay")
+		static let emptySearch = UIImage.image(named: "TrackersPlaceholderViewEmptySearch")
+	}
+
+	struct TabBar {
+		static let trackers = UIImage.image(named: "TrackersTabBarItem")
+		static let statistics = UIImage.image(named: "StatisticsTabBarItem")
+	}
+}
+
+extension UIImage {
+	static func image(named: String) -> UIImage {
+		guard let image = UIImage(named: named) else { fatalError("Cannot load image for name: \(named)") }
+		return image
 	}
 }
