48b7eaa2064ffc8c11c9c2fe129f76852045e7e5
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-07-07 12:59:21 +0700

    feat(new-category-screen): init for new category screen

---
+++ Tracker/Presentation/TrackerAdding/TrackerNewCategory/TrackerNewCategoryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TrackerNewCategoryViewController.swift
+//  Tracker
+//
+//  Created by Александр Бекренев on 07.07.2023.
+//
+
+import UIKit
+
+final class TrackerNewCategoryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Tracker/Presentation/TrackerAdding/TrackerNewCategory/TrackerNewCategoryViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TrackerNewCategoryViewModel.swift
+//  Tracker
+//
+//  Created by Александр Бекренев on 07.07.2023.
+//
+
+import Foundation
+
+final class TrackerNewCategoryViewModel {
+	
+}
