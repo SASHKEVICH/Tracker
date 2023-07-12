e6ee97d0fb7be528b7cee8abf4c5dbb73c8bc06f
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-07-12 21:27:51 +0700

    package(): added yandex-metrica and snapshot-testing packages

---
+++ Tracker/Helpers/YandexMetricaConfiguration.swift
@@ -0,0 +1,12 @@
+//
+//  YandexMetricaConfiguration.swift
+//  Tracker
+//
+//  Created by Александр Бекренев on 12.07.2023.
+//
+
+import Foundation
+
+struct YandexMetricaConfiguration {
+	static let apiKey = "4fe214c5-162e-40b9-8f31-3baf3e13e3ec"
+}
