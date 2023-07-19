9ac7b9eb806a28a7882fcb6b2d38f23d996c04f8
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-07-19 23:43:06 +0700

    fix(): using R.swift in images

--- Tracker/Helpers/UI/UIImage+Extensions.swift
+++ Tracker/Helpers/UI/UIImage+Extensions.swift
@@ -9,45 +9,38 @@ import UIKit
 
 extension UIImage {
 	struct Onboarding {
-		static let first = UIImage.image(named: "Onboarding_1")
-		static let second = UIImage.image(named: "Onboarding_2")
+		static let first = R.image.onboarding_1()
+		static let second = R.image.onboarding_2()
 	}
 
 	struct Categories {
-		static let selected = UIImage.image(named: "CategorySelectedIcon")
+		static let selected = R.image.categorySelectedIcon()
 	}
 
 	struct MainScreen {
-		static let addTracker = UIImage.image(named: "AddTrackerIcon")
+		static let addTracker = R.image.addTrackerIcon()
 		static let pinned = UIImage(systemName: "pin.fill",
 									withConfiguration: UIImage.SymbolConfiguration(font: .systemFont(ofSize: 12)))
 	}
 
 	struct Launch {
-		static let logo = UIImage.image(named: "PracticumLogo")
+		static let logo = R.image.practicumLogo()
 	}
 
 	struct Placeholder {
-		static let emptyForDay = UIImage.image(named: "TrackersPlaceholderViewEmptyForDay")
-		static let emptySearch = UIImage.image(named: "TrackersPlaceholderViewEmptySearch")
-		static let emptyStatistics = UIImage.image(named: "TrackersPlaceholderViewEmptyStatistics")
+		static let emptyForDay = R.image.trackersPlaceholderViewEmptyForDay()
+		static let emptySearch = R.image.trackersPlaceholderViewEmptySearch()
+		static let emptyStatistics = R.image.trackersPlaceholderViewEmptyStatistics()
 	}
 
 	struct TabBar {
-		static let trackers = UIImage.image(named: "TrackersTabBarItem")
-		static let statistics = UIImage.image(named: "StatisticsTabBarItem")
+		static let trackers = R.image.trackersTabBarItem()
+		static let statistics = R.image.statisticsTabBarItem()
 	}
 
 	struct CompleteTrackerButton {
 		static let completed = UIImage(systemName: "checkmark")
 		static let increase = UIImage(systemName: "plus")
 		static let decrease = UIImage(systemName: "minus")
-	}
-}
-
-extension UIImage {
-	static func image(named: String) -> UIImage {
-		guard let image = UIImage(named: named) else { fatalError("Cannot load image for name: \(named)") }
-		return image
 	}
 }
